2b2b9b1aa45eab6439450dc49d88e519a244b7db
alina <alinkina-pochaa@mail.ru>
2023-03-14 15:35:58 +0300

    add pw-config,report,npm scripts,jobs & pw dep

--- src/mail.e2e.spec.js
+++ src/mail.e2e.spec.js
@@ -1,34 +1,27 @@
-// const playwright = require('playwright');
-const playwright = require('@playwright/test');
-const chai = require('chai');
-const expect = chai.expect;
-
-let page, browser, context;
+const { test, expect, browser } = require('@playwright/test');
 
 const locators = {
-   registrationBtn:`[data-click-counter="75068995"]`,
-   searchBtn:`.search-container`,
-}
+  registrationBtn: `[data-click-counter="75068995"]`,
+  searchBtn: `.search-container`,
+};
 
-describe('mail home page', () => {
-  beforeEach(async function () {
-    browser = await playwright.chromium.launch({
-      headless: true,
-    });
+//test.describe.configure({ mode: 'parallel' });
+// test.describe.configure({
+// describe('mail home page', () => {
+  // beforeEach(async function () {
+    
+// await page.goto('https://mail.ru');
+// });
 
-    context = await browser.newContext();
-    page = await context.newPage('https://mail.ru/');
-    await page.goto('https://mail.ru');
-  });
+// afterEach(async function () {
+// await page.screenshot({ path: `schreenshots/${this.currentTest.title.replace(/\s+/g, '_')}.png` })
+// await browser.close();
+// });
 
-  afterEach(async function () {
-    // await page.screenshot({ path: `schreenshots/${this.currentTest.title.replace(/\s+/g, '_')}.png` })
-    await browser.close();
-  });
-
-  it('registration', async () => {
-    expect(
-      await page.locator(locators.registrationBtn).textContent()
-    ).to.equal('Регистрация');
-  });
+test.only('registration', async ({ page }) => {
+  await page.goto('https://mail.ru');
+  await expect(page.locator(locators.registrationBtn)).toHaveText(
+    'Регистрация'
+  );
 });
+// });
